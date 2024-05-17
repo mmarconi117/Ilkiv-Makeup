2257beb635c4b43cda1ec567b976db251e9b8244
Michael Marconi <michaelziga2@gmail.com>
2024-05-17 15:04:46 -0500

    fixed images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import './styles/styles.css';
 function Header() {
   return (
     <header className="bg-red-900 text-white py-4 mt-0 overflow-x-hidden">
-      <h1 className="text-yellow-500 text-left pl-4 text-5xl">Si Beauty Bar</h1>
+      <h1 className="text-yellow-500 text-left pl-4 text-5xl">𝕊𝕚 𝔹𝕖𝕒𝕦𝕥𝕪 𝔹𝕒𝕣</h1>
     </header>
   );
 }
