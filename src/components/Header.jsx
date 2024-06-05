8fae6016ef799522fe05697aeb54a2f3fb1c13d6
Michael Marconi <michaelziga2@gmail.com>
2024-06-05 12:54:33 -0500

    header fixed finally

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import './styles/styles.css';
 
 function Header() {
   return (
-    <header className="bg-opacity-60 text-white py-1 mt-0 overflow-x-hidden w-full">
-      <h1 className="text-black-500 text-opacity-10 text-center pl-4 text-5xl">𝕊𝕚 𝔹𝕖𝕒𝕦𝕥𝕪 𝔹𝕒𝕣</h1>
+    <header className="bg-gray-900 bg-opacity-60 text-white py-1 mt-0 overflow-x-hidden w-full">
+      <h1 className="text-maroon-500 text-opacity-100 text-center pl-4 text-5xl">𝕊𝕚 𝔹𝕖𝕒𝕦𝕥𝕪 𝔹𝕒𝕣</h1>
     </header>
   );
 }
