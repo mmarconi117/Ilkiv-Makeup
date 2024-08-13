ac512d28581f04f692c5bf671e948e53be467a6c
Michael Marconi <michaelziga2@gmail.com>
2024-08-13 00:08:31 -0500

    sofar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import './styles/styles.css';
 
 function Header() {
   return (
-    <header className="bg-gray-900 bg-opacity-60 text-white py-0 mt-15 overflow-x-hidden w-full max-w-[92%] mx-auto">
+    <header className="bg-gray-900 bg-opacity-60 text-white py-0 mt-15 overflow-x-hidden w-full max-w-[100%] mx-auto">
     <h1 className="text-maroon-500 text-opacity-100 text-center pl-4 text-5xl">𝕊𝕚 𝔹𝕖𝕒𝕦𝕥𝕪 𝔹𝕒𝕣</h1>
   </header>
 
