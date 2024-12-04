07c018ae648bd9d91370f1f20970e518c1dbc315
Michael Marconi <michaelziga2@gmail.com>
2024-12-03 22:59:17 -0600

    almost ready

--- src/backend/server.js
+++ src/backend/server.js
@@ -125,6 +125,6 @@ app.post("/api/login", async (req, res) => {
     }
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
