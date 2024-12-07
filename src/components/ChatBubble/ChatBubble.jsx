3d84b392f6d76917a4463292ee79773bc2137aa0
Michael Marconi <michaelziga2@gmail.com>
2024-12-07 09:37:29 -0600

    waiting on client

--- src/components/ChatBubble/ChatBubble.jsx
+++ src/components/ChatBubble/ChatBubble.jsx
@@ -18,7 +18,7 @@ function ChatBubble() {
     } else if (option === 'What I offer is...') {
       setChatResponse('I offer personalized beauty services tailored to your needs, ensuring you look and feel your best for weddings, proms, parties, photoshoots, and other special occasions.');
     } else if (option === 'Menu') {
-      setChatResponse('Here’s the menu: .');
+      setChatResponse('Here’s the menu: Nails, Hair, Makeup.');
     }
   };
 
