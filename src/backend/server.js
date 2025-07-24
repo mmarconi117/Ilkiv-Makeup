608df0bf02128c3a948cad0562c0faba5d3a95bb
Michael Marconi <michaelziga2@gmail.com>
2025-07-24 10:01:36 -0500

    working on errors then deploying

--- src/backend/server.js
+++ src/backend/server.js
@@ -237,15 +237,7 @@ app.post("/api/reset-password", async (req, res) => {
     }
 });
 
-// app.use('/api/chatbot', chatbotRoute);
 
-// app.use((err, req, res, next) => {
-//     res.status(err.status || 500).send({
-//         error: {
-//             message: err.message || "An internal server error occurred.",
-//         },
-//     });
-// });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
